3375895c475b945153ae7025a2de3a23ab3c2599
Tadeu Bastos <tadeu@ondular.net>
2021-07-14 11:43:08 +0100

    Convert post-conditions to idiomatic Python code

--- python/left_pad.py
+++ python/left_pad.py
@@ -3,8 +3,8 @@ import deal
 @deal.pre(lambda _: len(_.c) == 1)
 @deal.pre(lambda _: _.n in range(0, 0x10000))  # limit range to 16-bit integers
 @deal.ensure(lambda _: len(_.result) == max(_.n, len(_.s)))
-@deal.ensure(lambda _: all(_.result[i] == _.c for i in range(max(0, _.n - len(_.s)))))
-@deal.ensure(lambda _: all(_.result[max(0, _.n - len(_.s)) + i] == _.s[i] for i in range(len(_.s))))
+@deal.ensure(lambda _: all(x == _.c for x in _.result[:-len(_.s)]))
+@deal.ensure(lambda _: all(x[0] == x[1] for x in zip(reversed(_.result), reversed(_.s))))
 @deal.pure
 def left_pad(c: str, n: int, s: str) -> str:
     pre = c * max(0, n - len(s))
