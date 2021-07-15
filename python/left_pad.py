09d766354a235c3d3f705d85b89f3c63bb3f8fa2
Tadeu Bastos <tadeu@ondular.net>
2021-07-15 08:27:55 +0100

    Remove hardcoded limitation on values of `n`

--- python/left_pad.py
+++ python/left_pad.py
@@ -1,11 +1,14 @@
+import sys
 import deal
 
 @deal.pre(lambda _: len(_.c) == 1)
-@deal.pre(lambda _: _.n in range(0, 0x10000))  # limit range to 16-bit integers
+@deal.pre(lambda _: _.n >= 0)
 @deal.ensure(lambda _: len(_.result) == max(_.n, len(_.s)))
 @deal.ensure(lambda _: all(x == _.c for x in _.result[:-len(_.s)]))
 @deal.ensure(lambda _: all(x[0] == x[1] for x in zip(reversed(_.result), reversed(_.s))))
-@deal.pure
+@deal.raises(OverflowError)
+@deal.reason(OverflowError, lambda _: _.n - len(_.s) > sys.maxsize)
+@deal.has()
 def left_pad(c: str, n: int, s: str) -> str:
     pre = c * max(0, n - len(s))
     return f'{pre}{s}'
