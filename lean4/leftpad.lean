4a940cb3b87fb8603638a5db1d1690128094a4a6
Alex Chichigin <sad.ronin@gmail.com>
2025-03-28 11:37:45 +0400

    Leave only one leftpad.lean

---
+++ lean4/leftpad.lean
@@ -0,0 +1,80 @@
+/- More simplistic and straightforward version of the
+   leftpad specification and proofs in two versions:
+   for arbitrary lists in general and for strings specifically
+ -/
+
+variable {α : Type}
+
+def leftpad (n : Nat) (a : α) (l : List α) : List α :=
+  List.replicate (n - l.length) a ++ l
+
+#eval List.toString (leftpad 5 'b' (String.toList "ac"))
+
+theorem leftpad_length (n : Nat) (a : α) (l : List α) :
+    (leftpad n a l).length = max n (l.length) := by
+  simp [leftpad, Nat.max_def]
+  unfold ite
+  cases (Nat.decLe n (List.length l)) with
+  | isTrue h =>
+    dsimp
+    rw [Nat.sub_eq_zero_of_le]
+    . simp
+    . assumption
+  | isFalse h =>
+    dsimp
+    rw [Nat.sub_add_cancel]
+    apply Nat.le_of_lt (Nat.gt_of_not_le h)
+
+
+theorem prefix_concat [BEq α] [LawfulBEq α] (l m : List α) :
+    l.isPrefixOf (l ++ m) := by
+  induction l with
+  | nil => simp [List.isPrefixOf]
+  | cons x xs ih => simp [List.isPrefixOf, ih]
+
+theorem leftpad_prefix [BEq α] [LawfulBEq α] (n : Nat) (a : α) (l : List α) :
+    (List.replicate (n - l.length) a).isPrefixOf (leftpad n a l) := by
+  simp [leftpad]
+
+
+theorem suffix_concat [BEq α] [LawfulBEq α] (l m : List α) :
+    m.isSuffixOf (l ++ m) := by
+  unfold List.isSuffixOf
+  rw [List.reverse_append]
+  apply prefix_concat (List.reverse m) (List.reverse l)
+
+theorem leftpad_suffix [BEq α] [LawfulBEq α] (n : Nat) (a : α) (l : List α) :
+    l.isSuffixOf (leftpad n a l) := by
+  simp [leftpad]
+
+
+namespace Strings
+
+def leftpad (n : Nat) (a : Char) (s : String) : String :=
+  "".pushn a (n - s.length) ++ s
+
+#eval leftpad 5 'b' "ac"
+
+theorem leftpad_length (n: Nat) (a: Char) (s: String)
+    : (leftpad n a s).length = max n (s.length) :=
+  by simp [leftpad, Nat.sub_add_eq_max]
+
+theorem String.data_pushn (s: String) (c: Char) (n: Nat)
+    : (s.pushn c n).data = s.data ++ (List.replicate n c) := by
+  unfold String.pushn
+  refine n.recOn ?z ?s
+  case z => simp [Nat.repeat]
+  case s =>
+    intro n (ih: (Nat.repeat (·.push c) n s).data = s.data ++ List.replicate n c)
+    simp [Nat.repeat, ih]
+    exact Eq.symm List.replicate_succ'
+
+theorem leftpad_prefix (n: Nat) (a: Char) (s: String)
+    : (List.replicate (n - s.length) a) <+: (leftpad n a s).data := by
+  unfold leftpad
+  simp [String.data_append, String.data_pushn]
+
+theorem leftpad_suffix (n: Nat) (a: Char) (s: String)
+    : s.data <:+ (leftpad n a s).data := by
+  unfold leftpad
+  simp [String.data_append, String.data_pushn]
